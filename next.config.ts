9559c3aaabafee54356d1a872e596d6a495540bd
Fantsry <admin@MacBook-Pro-Admin.local>
2026-07-02 14:03:45 +0700

    config biar bisa di lokal image

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**',
         port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
         pathname: '/**',
       },
     ],
